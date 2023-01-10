b9ad5811e7ad1c84be380b5c8fea8d196e1cf583
Sebastián Giraldo <joabgiraldo@gmail.com>
2023-01-09 22:13:52 -0500

    Project Completed!

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
   const {data} = await axios.get(url, {
     headers: {
       "X-RapidAPI-Key": process.env.RAPID_API,
-      "X-RapidAPI-Host": "bayut.p.rapidapi.com",
+      "X-RapidAPI-Host": process.env.RAPID_HOST,
     },
   });
 
