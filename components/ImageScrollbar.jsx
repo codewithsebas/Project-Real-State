653a91b753ff221eab111b5a09e6e313837c8bd0
Sebastián Giraldo <joabgiraldo@gmail.com>
2023-01-09 21:32:58 -0500

    Finished Project!

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -39,8 +39,8 @@ export default function ImageSrollbar({ data }) {
   return (
     <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} style={{ overflow: 'hidden' }} >
       {data.map((item) => (
-        <Box width='910px' itemId={item.id} overflow='hidden' p='1'>
-          <Image placeholder="blur" blurDataURL={item.url} src={item.url} width={1000} height={500}  sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px" />
+        <Box width='910px' key={item.id} itemID={item.id} overflow='hidden' p='1'>
+          <Image placeholder="blur" blurDataURL={item.url} src={item.url} width={1000} height={500}  sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px" alt='State' />
         </Box>
       ))}
     </ScrollMenu>
