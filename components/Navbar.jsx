fde276e3cb3986834bf4966db5d4494e340f3803
Sebastian Giraldo <91224854+codewithsebas@users.noreply.github.com>
2023-01-20 23:10:32 -0500

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => (
                 <MenuItem icon={<BsSearch/>}>Search</MenuItem>
             </Link>
             <Link href="/search?purpose=for-sale" passHref>
-                <MenuItem icon={<FcAbout/>}>But Property</MenuItem>
+                <MenuItem icon={<FcAbout/>}>Buy Property</MenuItem>
             </Link>
             <Link href="/search?purpose=for-rent" passHref>
                 <MenuItem icon={<FiKey/>}>Rent Property</MenuItem>
